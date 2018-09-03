c97145de9adcf0be951c1abaf25860fe99fbc90d
furqan haroon <furqan.haroon@kwanso.com>
2018-09-03 17:45:55 +0500

    test

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000
 app.get('/testme', (req, res) => { 
   console.log("route file")
   return res.send({ success: true, message: "App Deployed" })
+  
 })
 
 app.listen(port, () => console.log('Example app listening on port '+port))
